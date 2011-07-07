5ce273ebfb17b0a97ccca5e610616b57075f9459
Brandon Nelson <theredraver@gmail.com>
2011-07-07 15:32:01 -0500

    Added helper methods for Character Classes and Races

--- Explorers/Models/CharacterClassList.cs
+++ Explorers/Models/CharacterClassList.cs
@@ -8,5 +8,20 @@ namespace WowDotNetAPI.Explorers.Models
 	public class CharacterClassList
 	{
 		public IEnumerable<CharacterClass> classes { get; set; }
+
+		public CharacterClass GetCharacterClassById(int id)
+		{
+			return classes.Single(c => c.id == id);
+		}
+
+		public CharacterClass GetCharacterClassByName(string name)
+		{
+			return classes.Single(c => c.name.Equals(name, StringComparison.InvariantCultureIgnoreCase));
+		}
+
+		public IEnumerable<CharacterClass> GetCharacterClassByPowerType(string powerType)
+		{
+			return classes.Where(c => c.powerType.Equals(powerType, StringComparison.InvariantCultureIgnoreCase));
+		}
 	}
 }
--- Explorers/Models/RaceList.cs
+++ Explorers/Models/RaceList.cs
@@ -8,5 +8,20 @@ namespace WowDotNetAPI.Explorers.Models
 	public class RaceList
 	{
 		public IEnumerable<Race> races { get; set; }
+
+		public Race GetRaceById(int id)
+		{
+			return races.Single(r => r.id == id);
+		}
+
+		public Race GetRaceByName(string name)
+		{
+			return races.Single(r => r.name.Equals(name, StringComparison.InvariantCultureIgnoreCase));
+		}
+
+		public IEnumerable<Race> GetRacesBySide(string side)
+		{
+			return races.Where(r => r.side.Equals(side, StringComparison.InvariantCultureIgnoreCase));
+		}
 	}
 }
